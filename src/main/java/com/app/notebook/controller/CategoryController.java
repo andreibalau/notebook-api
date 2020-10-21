9ded4b2e87ba140a5490d74603f420eacdff897e
Catalin <icatalin201@gmail.com>
2020-10-21 21:51:45 +0300

    add initial endpoints

---
+++ src/main/java/com/app/notebook/controller/CategoryController.java
@@ -0,0 +1,11 @@
+package com.app.notebook.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/** notebook-api Created by Catalin on 10/21/2020 */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/api/v1/categories")
+public class CategoryController {}
